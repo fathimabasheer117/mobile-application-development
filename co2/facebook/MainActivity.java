56ac7d69a31d774474572c2bf2eb07a1df184559
Fathima Basheer <94823829+fathimabasheer117@users.noreply.github.com>
2022-09-27 11:21:30 +0530

    Create MainActivity.java

---
+++ co2/facebook/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.facebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void create(View view){
+        Intent crt=new Intent(this,Activity2.class);
+        startActivity(crt);
+    }
+}
